f0f67c7271e1d38f3c7c7d8be47cb362b7e595d4
ImLeugim <anarchxzx@gmail.com>
2023-04-06 08:57:30 -0700

    style: style home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import { Heading, Stack, Image } from "@chakra-ui/react";
+import { Heading, Stack, Image, Text } from "@chakra-ui/react";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <Stack textAlign="center">
       <Heading>Hello Synthesis!</Heading>
-      <Stack align="center">
+      <Stack align="center" spacing={10} pt={10}>
+        <Text>Check out our new memory game below</Text>
         <Link href="/synthesis-memory">
           <Image
-            boxSize="2xl"
             src="/images/synMemory/synMemory.gif"
             alt="Synthesis Logo"
             objectFit="contain"
